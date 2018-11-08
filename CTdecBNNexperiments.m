42f0f146b906cf74788b80f40bb77e0beb0f2c20
Takamichi Miyata <takamichi.miyata@gmail.com>
2018-11-08 09:39:40 +0900

    Update CTdecBNNexperiments.m

--- CTdecBNNexperiments.m
+++ CTdecBNNexperiments.m
@@ -7,6 +7,20 @@
 % "Cartoon-Texture Image Decomposition Using Blockwise Low-Rank Texture Characterization,"
 % IEEE Transactions on Image Processing, vol. 23, no. 3, pp. 1128-1142, 2014.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  Copyright 2014 Shunsuke Ono 
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all;
 close all;
